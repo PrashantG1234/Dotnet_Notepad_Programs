5dc28481e81d33b474a383aa011d11f1088d40e0
Prashant Kumar Gupta <prashantkumar_gupta@epam.com>
2025-01-25 22:52:37 +0530

    Created a # pattern in this file

---
+++ XShape.cs
@@ -0,0 +1,53 @@
+using System;
+public class XShape{
+    public static void Main(){
+        // for half hash patter
+        for(int i=0;i<4;i++){
+            //for initial space 
+            for(int j=0;j<i;j++){
+                Console.Write(" ");
+            }
+            //for initial hashes
+            for(int j=0;j<=i;j++){
+                Console.Write("#");
+            }
+            //for secondary spaces
+            for(int j=6;j>i*2;j--){
+                Console.Write(" ");
+            }
+            //for opposite secondary spaces
+            for(int j=6;j>i*2;j--){
+                Console.Write(" ");
+            }
+            //for opposite hashes
+            for(int j=0;j<=i;j++){
+                Console.Write("#");
+            }
+            Console.WriteLine();
+        }
+        //for another half hash pattern
+         for(int i=0;i<4;i++){
+            //for initial space 
+            for(int j=3;j>i;j--){
+                Console.Write(" ");
+            }
+            //for initial hashes
+            for(int j=4;j>i;j--){
+                Console.Write("#");
+            }
+            //for secondary spaces
+            for(int j=2*i;j>0;j--){
+                Console.Write(" ");
+            }
+            //for opposite secondary spaces
+            for(int j=2*i;j>0;j--){
+                Console.Write(" ");
+            }
+            //for opposite hashes
+            for(int j=4;j>i;j--){
+                Console.Write("#");
+            }
+            Console.WriteLine();
+        }   
+    }
+}
